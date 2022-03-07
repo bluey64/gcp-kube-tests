c725854978f4f032cf3571c4631820a8d7b06f2e
bluey64 <mark.seida@datadoghq.com>
2022-03-07 14:11:59 -0600

    Update Kube-create.sh

--- Kube-create.sh
+++ Kube-create.sh
@@ -17,7 +17,7 @@ fi
 if [ -n "$CLUSTERNAME" -a "$PROJECT" ]
 then
 echo "Creating Cluster"
-gcloud container clusters create $CLUSTERNAME --zone us-central1-a --node-locations us-central1-a --num-nodes=3 &> /dev/null
+gcloud container clusters create $CLUSTERNAME --zone us-central1-a --machine-type=n1-standard-8 --node-locations us-central1-a --num-nodes=3 &> /dev/null
 
 echo "Setting up kubectl"
 gcloud container clusters get-credentials $CLUSTERNAME --zone us-central1-a --project $PROJECT &> /dev/null
